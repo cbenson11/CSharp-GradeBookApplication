a8f0cb66245874e6d06e1f78e5c4bba4647df0f8
cbenson11 <67032200+cbenson11@users.noreply.github.com>
2020-08-02 08:20:17 -0500

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -10,6 +11,42 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            List<double> gradeList = new List<double>();
+            foreach (var student in Students)
+            {
+                gradeList.Add(student.AverageGrade);
+            }
+            var descendingOrder = gradeList.OrderByDescending(i => i);
+            int indx = 0;
+            foreach (var grade in descendingOrder)
+            {
+                indx++;
+                if (grade >= averageGrade)
+                {
+                    float rank = indx / Students.Count;
+                    if (rank <= 0.20)
+                        return 'A';
+                    else
+                    if (rank <= 0.40)
+                        return 'B';
+                    else
+                    if (rank <= 0.60)
+                        return 'C';
+                    else
+                    if (rank <= 0.80)
+                        return 'D';
+                }
+            }
+            //return base.GetLetterGrade(averageGrade);
+            return 'F';
         }
     }
 }
